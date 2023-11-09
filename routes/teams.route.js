f64d09a528c7252e5e945a6fd525bfb16dda4701
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-11-09 18:13:14 +0100

    Create 'post' route for teams and test

--- routes/teams.route.js
+++ routes/teams.route.js
@@ -6,8 +6,17 @@ router.get('/', async (req, res)=>{
         const teams = await TeamModel.find();
         res.json(teams);
     } catch (error) {
-        res.status(500).json({error: "No ha sido posible obtener los equipos."});
+        res.status(500).json({error: error});
     }
-})
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const newTeam = await TeamModel.create(req.body);
+        res.json(newTeam);
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+});
 
 module.exports = router;
--- test/teams.route.spec.js
+++ test/teams.route.spec.js
@@ -2,8 +2,21 @@ require('dotenv').config();
 const app = require('../app');
 const request = require('supertest');
 const mongoose = require('mongoose');
+const TeamModel = require('../models/team.model');
 
 describe('Test on teams API', ()=>{
+
+    const newTeam = {
+        name: "Prueba Equipo",
+        acronym: "PE",
+        PG: 0,
+        PE: 0,
+        PP: 0,
+        GF: 0,
+        GC: 0,
+        shield: "Sin escudo",
+        players: []
+    }
 
     beforeAll(async ()=>{
         await mongoose.connect(process.env.URL_MONGODB);
@@ -11,7 +24,7 @@ describe('Test on teams API', ()=>{
 
     afterAll(async ()=>{
         await mongoose.disconnect();
-    })
+    });
 
     describe('GET /api/teams', ()=>{
 
@@ -21,7 +34,7 @@ describe('Test on teams API', ()=>{
             response = await request(app).get('/api/teams').send();
         });
 
-        it('Route works', async ()=>{
+        it('Route "GET" works', async ()=>{
             expect(response.status).toBe(200);
             expect(response.headers['content-type']).toContain('json');
         });
@@ -30,5 +43,46 @@ describe('Test on teams API', ()=>{
             expect(response.body).toBeInstanceOf(Array);
         })
         
-    })
+    });
+
+    describe('POST /api/teams', ()=>{
+
+        const wrongTeam = {
+            name: "Prueba Equipo",
+            acronym: "PE",
+            PG: 0,
+            PE: 0,
+            PP: 0,
+            GF: 0,
+            GC: 0,
+            shield: "Sin escudo",
+            players: {}
+        }
+
+        afterAll(async ()=>{
+            await TeamModel.deleteMany({ name: "Prueba Equipo"});
+        });
+
+        it('Route "POST" works', async ()=>{
+            const response = await request(app).post('/api/teams').send(newTeam);
+
+            expect(response.status).toBe(200);
+            expect(response.headers['content-type']).toContain('json');
+        });
+
+        it('Should add new team', async ()=>{
+            const response = await request(app).post('/api/teams').send(newTeam);
+
+            expect(response.body._id).toBeDefined();
+            expect(response.body.name).toBe(newTeam.name);
+        });
+
+        it('Should not add new team', async ()=>{
+            const response = await request(app).post('/api/teams').send(wrongTeam);
+
+            expect(response.status).toBe(500);
+            expect(response.body.error).toBeDefined();
+        });
+    });
+
 })
