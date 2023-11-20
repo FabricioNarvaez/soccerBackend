22a71485d489f9b5a1d388cca07dc02ef2acacb3
KevinMalla <malla.kevin24@gmail.com>
2023-11-20 20:52:53 +0100

    Apply changes

--- controllers/coach.controller.js
+++ controllers/coach.controller.js
@@ -35,19 +35,6 @@ const controller = {
 			res.status(500).json({ error: error });
 		}
 	},
-	checkAuth: async (req, res, next) => {
-		try {
-			const { token } = req.body;
-			const tokenData = await veryfyToken(token);
-			if (tokenData.id) {
-				next();
-			} else {
-				res.status(401).send({ message: 'Unauthorized' });
-			}
-		} catch (error) {
-			res.status(401).send({ message: 'Unauthorized' });
-		}
-	},
 };
 
 module.exports = controller;
