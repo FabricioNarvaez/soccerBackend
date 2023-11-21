5630176cb16631570bb12f46548fd1b32fcc3b58
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-11-21 19:32:00 +0100

    [BAC-6] Create admin model

---
+++ controllers/coach.controller.js
@@ -0,0 +1,40 @@
+const CoachModel = require('../models/coach.model');
+const { encryptPassword, comparePassword } = require('../helpers/handleBcrypt');
+const { generateToken, veryfyToken } = require('../helpers/generateToken');
+
+const controller = {
+	registerCoach: async (req, res) => {
+		try {
+			const newCoach = req.body;
+			newCoach.password = await encryptPassword(newCoach.password);
+
+			const insertedCoach = await CoachModel.create(newCoach);
+			res.json(insertedCoach);
+		} catch (error) {
+			res.status(500).json({ error: error });
+		}
+	},
+	logInCoach: async (req, res) => {
+		try {
+			const { userName, password } = req.body;
+			const registeredCoach = await CoachModel.findOne({ userName });
+
+			if (!registeredCoach) {
+				return res.status(404).send({ message: 'Coach not found' });
+			}
+
+			const checkPassword = await comparePassword(password, registeredCoach.password);
+
+			if (!checkPassword) {
+				return res.status(401).send({ message: 'Unauthorized' });
+			}
+
+			const tokenSession = await generateToken(registeredCoach);
+			res.send({ data: registeredCoach, token: tokenSession });
+		} catch (error) {
+			res.status(500).json({ error: error });
+		}
+	},
+};
+
+module.exports = controller;
---
+++ models/admin.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const adminSchema = new Schema({
+	name: String,
+	password: String,
+	userName: String,
+});
+
+module.exports = mongoose.model('admins', adminSchema);
