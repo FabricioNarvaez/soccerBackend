a4956ad207862c5fe2db8acd0120fc0d23d3dd27
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-11-21 19:39:22 +0100

    [BAC-6] Create common model

--- models/admin.model.js
+++ models/admin.model.js
@@ -1,10 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const userSchema = require('./common.model');
 
-const adminSchema = new Schema({
-	name: String,
-	password: String,
-	userName: String,
-});
+const adminSchema = new Schema(userSchema);
 
 module.exports = mongoose.model('admins', adminSchema);
--- models/coach.model.js
+++ models/coach.model.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const userSchema = require('./common.model');
 
-const coachSchema = new Schema({
-	name: String,
-	password: String,
-});
+const coachSchema = new Schema(userSchema);
 
 module.exports = mongoose.model('coaches', coachSchema);
