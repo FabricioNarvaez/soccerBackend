14180dd315d143ed346d4e9d022ac5d09ca118a2
FabricioNarvaez <narvaezfabricio38@gmail.com>
2023-11-23 19:14:42 +0100

    [BAC-7] Refactor coach controller

--- controllers/coach.controller.js
+++ controllers/coach.controller.js
@@ -1,14 +1,13 @@
 const CoachModel = require('../models/coach.model');
 const { registerUser, loginUser } = require('./common.controllers');
 
-const controller = {
-	registerCoach: async (req, res) => {
-		registerUser(req, res, CoachModel);
-	},
-	logInCoach: async (req, res) => {
-		const errorMessage = 'Coach not found';
-		loginUser(req, res, CoachModel, errorMessage);
-	},
+const registerCoach = async (req, res) => {
+	registerUser(req, res, CoachModel);
 };
 
-module.exports = controller;
+const logInCoach = async (req, res) => {
+	const errorMessage = 'Coach not found';
+	loginUser(req, res, CoachModel, errorMessage);
+};
+
+module.exports = { registerCoach, logInCoach };
