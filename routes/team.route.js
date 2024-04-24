43ad51ab617172ce73791de5e2c38bc5d57e996a
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-24 15:30:33 +0200

    [BAC-39] Add getTeamById controller

--- routes/team.route.js
+++ routes/team.route.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const { getTeams, createTeam, updateTeam, deleteTeam } = require('../controllers/team.controller');
+const { getTeams, getTeamById, createTeam, updateTeam, deleteTeam } = require('../controllers/team.controller');
 
 router.get('/all', getTeams);
+router.get('/team/:id', getTeamById);
 router.post('/create', createTeam);
 router.put('/:id', updateTeam);
 router.delete('/:id', deleteTeam);
