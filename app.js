04e387805b93afa730e096dd16dc7b83c2fed506
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-01-17 12:42:18 +0100

    Update cors options

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'https://soccer-front.vercel.app/', 
+    'https://soccer-front.vercel.app', 
     'http://localhost:5173'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'],
   credentials: true,
   optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
+  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
