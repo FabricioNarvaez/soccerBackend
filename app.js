b5379be7c628b896fc83c6f48158f58d58c62d55
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-01-17 12:33:07 +0100

    Solve issue with cors

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: [
+    'https://soccer-front.vercel.app/', 
+    'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+  credentials: true,
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
