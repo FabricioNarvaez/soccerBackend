949ece8afa742d9b181166263562c05a8d8153cf
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-12 11:50:29 +0100

    Create seed to add first admin user

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const admin = await prisma.user.upsert({
+    where: { email: 'admin@soccer.com' },
+    update: {},
+    create: {
+      email: 'admin@soccer.com',
+      password: 'password123', // TODO: Replace with bcrypt
+      name: 'Admin de Prueba',
+      role: 'ADMIN',
+    },
+  });
+
+  console.log({ admin });
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
