5dd95a0e3d4ef5c3563ca3363d2cc8151af5aaa5
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-12 11:38:36 +0100

    - Updated @prisma/client and prisma packages to version 5.10.2. - Removed prisma.config.ts as it is no longer needed. - Updated schema.prisma to use environment variable for DATABASE_URL. - Refactored PrismaService to remove datasource URL.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,12 +6,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    super({
-      datasourceUrl: "postgresql://admin:soccer_password@localhost:5432/soccer_db?schema=public",
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
   }
