f933a57d1703fb2a88f7d53685c752560160ddac
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-13 13:17:52 +0100

    Add class-transformer and class-validator, and implement ValidationPipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
