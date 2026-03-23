a2440c3d14d6696c4c731182fff8a03d8fe2b068
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-23 13:10:06 +0100

    Add validations for team Dto

--- src/teams/dto/create-team.dto.ts
+++ src/teams/dto/create-team.dto.ts
@@ -1,7 +1,23 @@
+import { IsInt, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
 export class CreateTeamDto {
+  @IsString()
+  @IsNotEmpty({ message: 'El nombre del equipo es obligatorio.' })
   name: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'El acrónimo del equipo es obligatorio.' })
   acronym: string;
+
+  @IsInt()
+  @IsNotEmpty({ message: 'Debe proporcionar el ID del entrenador del equipo.' })
   coachId: number;
+
+  @IsString()
+  @IsOptional()
   shield?: string;
+
+  @IsString()
+  @IsOptional()
   color?: string;
 }
