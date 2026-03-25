46fd483ff940cf9c22886ae9ba896c2b9ed1b5b2
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-25 12:09:41 +0100

    Create service to create new match

--- src/matches/matches.service.ts
+++ src/matches/matches.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateMatchDto } from './dto/create-match.dto';
 import { UpdateMatchDto } from './dto/update-match.dto';
+import { PrismaService } from '../prisma/prisma.service';
+import { Match } from '@prisma/client';
 
 @Injectable()
 export class MatchesService {
-  create(createMatchDto: CreateMatchDto) {
-    return 'This action adds a new match';
+  constructor(private prisma: PrismaService) {}
+
+  async create(createMatchDto: CreateMatchDto): Promise<Match> {
+    if(createMatchDto.homeTeamId === createMatchDto.awayTeamId) {
+      throw new BadRequestException('Un equipo no puede jugar contra sí mismo.');
+    }
+
+    return await this.prisma.match.create({
+      data: createMatchDto,
+    });
   }
 
   findAll() {
