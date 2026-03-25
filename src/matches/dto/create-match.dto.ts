ed9dfee25e29218698768f7cb293bdcebe3e569a
FabricioNarvaez <narvaezfabricio38@gmail.com>
2026-03-25 12:09:28 +0100

    Create DTO with validations

--- src/matches/dto/create-match.dto.ts
+++ src/matches/dto/create-match.dto.ts
@@ -1,1 +1,33 @@
-export class CreateMatchDto {}
+import { IsDateString, IsNotEmpty, IsOptional, IsString, IsInt, Min, IsBoolean} from "class-validator";
+
+export class CreateMatchDto {
+  @IsDateString()
+  @IsNotEmpty()
+  date: string;
+
+  @IsString()
+  @IsOptional()
+  location?: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  homeTeamId: number;
+
+  @IsInt()
+  @IsNotEmpty()
+  awayTeamId: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  homeGoals?: number;
+
+  @IsInt()
+  @Min(0)
+  @IsOptional()
+  awayGoals?: number;
+
+  @IsBoolean()
+  @IsOptional()
+  isFinished?: boolean;
+}
